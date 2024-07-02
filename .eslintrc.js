dd507a507bd163c07f1a1725cd7cee5362bd0a09
Timur <Stafr95@mail.ru>
2024-07-02 22:51:00 +0500

    Customizing .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: [
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-		'prettier/@typescript-eslint',
-		'prettier/react',
-	],
-	plugins: ['react', '@typescript-eslint', 'prettier'],
-	rules: {
-		'prettier/prettier': 'error',
-		'@typescript-eslint/explicit-function-return-type': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
